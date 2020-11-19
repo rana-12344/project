333ba2483644344630cfd937df7eb220d59a8a7c
Taif <74107163+Taif50@users.noreply.github.com>
2020-11-19 11:16:23 +0300

    Add files via upload

---
+++ user.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+echo "welcome ". $_SESSION['u_name'];
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>....</title>
+    <link rel="stylesheet" href="project.css">
+  </head>
+  <body>
+    <div class="topnav">
+      <a class="active" href="home.html">الرئيسية</a>
+      <a href="favorite.html">المفضلة</a>
+      <a href="cities.html">الفعاليات</a>
+      <a href="signIn.php">تسجيل الاشتراك</a>
+      <a href="logIn.php">تسجيل الدخول</a>
+    </div>
+
+    <br><br>
+  </body>
+</html>
