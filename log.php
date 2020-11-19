87512a5e73989e059ae73d7d5c19cd377e8e2f32
Taif <74107163+Taif50@users.noreply.github.com>
2020-11-19 11:17:19 +0300

    Update log.php

--- log.php
+++ log.php
@@ -1,25 +1,27 @@
 <?php
-$u_email = $_POST['u_email'];
+$u_name = $_POST['u_name'];
 $u_password = $_POST['u_password'];
 
 $con = new mysqli("localhost","root","","website");
 if($con->connect_error){
   die("Failed connection :".$con->connect_error);
-} else {
-  $stmt = $con->prepare("select * from users_info where u_email = ?");
-  $stmt->bind_param("s",$u_email);
+} else
+  $stmt = $con->prepare("select * from users_info where u_name = ?");
+  $stmt->bind_param("s",$u_name);
   $stmt->execute();
   $stmt_result = $stmt->get_result();
 
-  if($stmt_result->num_rows > 0){
-    $data = $stmt_result->fetch_assoc();
-    if($data['u_password'] === $u_password){
-      echo "تم تسجيل الدخول";
-    } else {
-      echo "خطأ في كلمة المرور او الايميل";
-    }
-  } else {
-    echo "خطأ في كلمة المرور او الايميل";
-  }
-}
+     if($stmt_result->num_rows > 0){
+    $data = $stmt_result->fetch_assoc();}
+       if($data['u_password'] === $u_password){
+
+        $sname=$_POST['u_name'];
+        $spass=$_POST['u_password'];
+           if($sname == $u_name and $spass == $u_password){
+          session_start();
+          $_SESSION['u_name']=$sname;
+          header("Location:user.php");
+          }
+      }else  {echo "لقد تم استخدام هذا الايميل او الاسم <br> لاعادة المحاولة <a href='login.php'>اضغط هنا</a>";}
+
  ?>
