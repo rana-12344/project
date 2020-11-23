254bb10b63fed74ec0aa14df35dbe324e4b0cef1
Taif <74107163+Taif50@users.noreply.github.com>
2020-11-23 20:31:11 +0300

    Update user.php

--- user.php
+++ user.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-echo "welcome ". $_SESSION['u_name'];
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -8,6 +7,24 @@ echo "welcome ". $_SESSION['u_name'];
     <meta charset="utf-8">
     <title>....</title>
     <link rel="stylesheet" href="project.css">
+    <style media="screen">
+    table, th, td {
+border: 1px solid black;
+border-collapse: collapse;
+background-color: #f1f1c1 ;
+}
+table, th, td {
+  border: 2px solid black;
+  font-size: 20px;
+  text-align: center;
+}
+th, td {
+  padding: 10px;
+}
+table {
+  border-spacing: 5px;
+}
+    </style>
   </head>
   <body>
     <div class="topnav">
@@ -15,8 +32,49 @@ echo "welcome ". $_SESSION['u_name'];
       <a href="cities.html">الفعاليات</a>
       <a href="signIn.php">تسجيل الاشتراك</a>
       <a href="logIn.php">تسجيل الدخول</a>
-    </div>
+    </div> <br><br><br><br>
+<center><?php
+ echo "<h1>"."welcome ". $_SESSION['u_name'],"</h1>";
+ ?>
+    <br><h2>الحجوزات</h2><br></center>
+    <table style="width:100%">
+  <tr>
+    <th>المهرجانات</th>
+    <th>المتاحف</th>
+    <th>المعالم السياحية</th>
+  </tr>
+  <tr>
+    <td> <a href="">مهرجان الجنادرية</a> </td>
+    <td> <a href="">قصر المربع </a> </td>
+    <td> <a href=""> مدينة الثلج</a> </td>
+  </tr>
+  <tr>
+    <td> <a href=""> مهرجان الملك عبد العزيز للإبل</a> </td>
+    <td> <a href="">متحف الاثار</a> </td>
+    <td> <a href="">برج المملكة </a> </td>
+  </tr>
+  <tr>
+    <td> <a href=""> ليالي الرياض</a> </td>
+    <td> <a href="">متحف جوي </a> </td>
+    <td> <a href="">برج الفيصلي</a> </td>
+  </tr>
+  <tr>
+    <td> <a href=""> مهرجان الدرعية للفروسية</a> </td>
+    <td> <a href="">قصر المصمك </a> </td>
+    <td> <a href="">برج رافال</a> </td>
+  </tr>
+  <tr>
+    <td> <a href=""> مهرجان الملك عبدالعزيز للصقور</a> </td>
+    <td> <a href="">قصر الحمرا</a> </td>
+    <td> <a href="">حديقة الملك عبدالله</a> </td>
+  </tr>
+  <tr>
+    <td> <a href="">سماء فانتوم</a> </td>
+    <td></td>
+    <td> <a href=""> رياض سفاري</a> </td>
+  </tr>
 
-    <br><br>
+</table>
+
   </body>
 </html>
